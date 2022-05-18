9129ce7b2ee1646d38c35c93e1f4c605f81d9814
roopeak <roopeak@protonmail.com>
2022-05-18 11:35:42 +0300

    Tried to find solution to grid

--- scripts.js
+++ scripts.js
@@ -1,9 +1,42 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16 , 1fr)";
-board.style.gridTemplateRows = "repeat(16 , 1fr)";
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = 'blue';
-    board.insertAdjacentElement("beforeend", square);
+// Creates a default grid sized 16x16
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
 }
+
+// Takes (rows, columns) input and makes a grid
+function makeRows(rowNum) {
+
+    // Creates rows
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+
+// Creates columns
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
+
+
+
+// let board = document.querySelector(".board");
+// board.style.gridTemplateColumns = "repeat(16 , 1fr)";
+// board.style.gridTemplateRows = "repeat(16 , 1fr)";
+
+// for (let i = 0; i < 256; i++) {
+//     let square = document.createElement("div");
+//     square.style.backgroundColor = 'blue';
+//     board.insertAdjacentElement("beforeend", square);
+// }
