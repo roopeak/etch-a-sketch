33b29a6f9ae43b269efdf7e1ad1b62b2bbca8bd3
Roope Kärkkäinen <roopeak@protonmail.com>
2025-01-10 08:43:55 +0200

    Create hover effect to grids

--- scripts.js
+++ scripts.js
@@ -4,16 +4,8 @@ for (let i = 0; i < 16 * 16; i++) {
   const gridItem = document.createElement('div');
   gridItem.classList.add('grid-item');
   gridContainer.appendChild(gridItem);
+
+	gridItem.addEventListener("mouseover", () => {
+		gridItem.style.cssText = "background-color: black;";
+	});
 }
-
-// function generateGrids() {
-//   for (let i = 0; i < 16; i++) {
-// 		for (let j = 0; j < 16; j++) {
-// 			const grid = document.createElement('div');
-// 			grid.classList.add('grid');
-// 			gridContainer.appendChild(grid);
-// 		}
-// 	}   
-// }
-
-// generateGrids();
