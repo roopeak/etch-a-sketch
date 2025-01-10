c6befc92391762f93795184423fe662dcd1c74ae
Roope Kärkkäinen <roopeak@protonmail.com>
2025-01-10 11:24:28 +0200

    Generate grid size from input

--- scripts.js
+++ scripts.js
@@ -1,11 +1,41 @@
 const gridContainer = document.querySelector('.grid-container');
+const sizeBtn = document.querySelector('.size-btn');
 
-for (let i = 0; i < 16 * 16; i++) {
-  const gridItem = document.createElement('div');
-  gridItem.classList.add('grid-item');
-  gridContainer.appendChild(gridItem);
+function generateGrid(size) {
+	console.log(size);
+	gridContainer.innerHTML = "";
+	gridContainer.style.display = "grid";
+	gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+	gridContainer.style.width = "600px";
+	gridContainer.style.height = "600px";
 
-	gridItem.addEventListener("mouseover", () => {
-		gridItem.style.cssText = "background-color: black;";
-	});
+	for (let i = 0; i < size * size; i++) {
+		const gridItem = document.createElement("div");
+		gridItem.classList.add("grid-item");
+		gridItem.style.border = "1px solid black";
+		gridContainer.appendChild(gridItem);
+
+		gridItem.addEventListener("mouseover", () => {
+				gridItem.style.backgroundColor = "black";
+		});
+	}
 }
+
+function askInput() {
+	while (true) {
+		let input = prompt("Type the size of the grid (1-100):");
+		input = Number(input);
+
+		if (!isNaN(input) && input >= 1 && input <= 100) {
+			return input;
+		} else {
+			alert('Grid size must be between 1 and 100!');
+		}
+	}
+}
+
+sizeBtn.addEventListener('click', () => {
+	const gridSize = askInput();
+	generateGrid(gridSize);
+})
