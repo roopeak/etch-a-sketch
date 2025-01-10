8e9d039f41f55b146259e8bd9d2fb053b78da35c
Roope Kärkkäinen <roopeak@protonmail.com>
2025-01-10 08:25:35 +0200

    Create grids

--- scripts.js
+++ scripts.js
@@ -1,2 +1,19 @@
 const gridContainer = document.querySelector('.grid-container');
 
+for (let i = 0; i < 16 * 16; i++) {
+  const gridItem = document.createElement('div');
+  gridItem.classList.add('grid-item');
+  gridContainer.appendChild(gridItem);
+}
+
+// function generateGrids() {
+//   for (let i = 0; i < 16; i++) {
+// 		for (let j = 0; j < 16; j++) {
+// 			const grid = document.createElement('div');
+// 			grid.classList.add('grid');
+// 			gridContainer.appendChild(grid);
+// 		}
+// 	}   
+// }
+
+// generateGrids();
